1ce57819ba6b58402e60be8756ee4fdc27eb517e
Brandon <brand28125@gmail.com>
2018-08-24 14:18:43 +0530

    deleted

--- ChildBrowserTest/ChildBrowserTest/Program.cs
+++ ChildBrowserTest/ChildBrowserTest/Program.cs
@@ -11,12 +11,7 @@ namespace ChildBrowserTest
         static void Main(string[] args)
         {
             
-            Console.WriteLine("Bye Child");
-            Console.WriteLine("1");
-            Console.WriteLine("2");
-            Console.WriteLine("3");
-            Console.WriteLine("4");
-            Console.WriteLine("5");
+           
             //new comment
         }
     }
